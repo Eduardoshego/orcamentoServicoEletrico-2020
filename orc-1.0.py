83eff94f3bc30afe01c14956f8b026b9a4e65a50
Eduardo <eduardoshego1@gmail.com>
2020-06-17 22:17:52 -0300

    Implementando funçoes de rotinas

--- orc-1.0.py
+++ orc-1.0.py
@@ -1,4 +1,4 @@
-print('Programa de orçamento de serviços eletricos /2020')
+
 iluminacao = [
 
     {
@@ -251,7 +251,9 @@ pontoUtilizacao = [
     {
         'item':'CÂMERAS CFTV 3 CÂMERAS + DVR',
         'preco':150.00,
-        'preco2':225.00
+        'preco2':225.00,
+        'preco1':0,
+        'preco3':0
     },
     {
         'item':'PORTAO ELETRÔNICO DESLIZANTE',
@@ -290,142 +292,410 @@ pontoUtilizacao = [
         'preco3':204.00
     }
 ]
-qdc = {}
-passagemcabo20m = {}
-soluceletrica = {}
+qdc = [
+    {
+        'item':'DISJUNTOR MONOFÁSICO',
+        'preco':30.00,
+        'preco1':45.00,
+        'preco2':24.00,
+        'preco3':36.00
+    },
+    {
+        'item':'DISJUNTOR BIFÁSICO',
+        'preco':50.00,
+        'preco1':75.00,
+        'preco2':40.00,
+        'preco3':60.00
+    },
+    {
+        'item':'DISJUNTOR TRIFÁSICO',
+        'preco':70.00,
+        'preco1':105.00,
+        'preco2':56.00,
+        'preco3':84.00
+    },
+    {
+        'item':'IDR (INTERRUPTOR DIFERENCIAL RESIDUAL)',
+        'preco':90.00,
+        'preco1':135.00,
+        'preco2':72.00,
+        'preco3':108.00
+    },
+    {
+        'item':'DPS - DISPOSITIVO DE PROTEÇÃO CONTRA SURTOS',
+        'preco':70.00,
+        'preco1':105.00,
+        'preco2':56.00,
+        'preco3':84.00
+    },
+    {
+        'item':'BARRAMENTO PENTE MONOPOLAR NO QDC',
+        'preco':35.00,
+        'preco1':52.50,
+        'preco2':28.00,
+        'preco3':42.00
+    },
+    {
+        'item':'BARRAMENTO PENTE BIPOLAR NO QDC',
+        'preco':45.00,
+        'preco1':67.50,
+        'preco2':36.00,
+        'preco3':54.00
+    },
+    {
+        'item':'BARRAMENTO PENTE TRIPOLAR NO QDC',
+        'preco':55.00,
+        'preco1':82.50,
+        'preco2':44.00,
+        'preco3':66.00
+    },
+    {
+        'item':'BARRAMENTO DE NEUTRO e / ou TERRA',
+        'preco':50.00,
+        'preco1':75.00,
+        'preco2':40.00,
+        'preco3':60.00
+    },
+    {
+        'item':'CIRCUITO DE ATERRAMENTO C/ ATÉ 5 HASTES',
+        'preco':340.00,
+        'preco1':510.00,
+        'preco2':0,
+        'preco3':0
+    },
+    {
+        'item':'CONTATOR E/ OU RELÉ TÉRMICO',
+        'preco':110.00,
+        'preco1':165.00,
+        'preco2':88.00,
+        'preco3':132.00
+    },
+    {
+        'item':'INSTALAÇÃO E MONTAGEM QDC (6 CIRCUITOS + DR + DPS)',
+        'preco':370.00,
+        'preco1':555.00,
+        'preco2':296.00,
+        'preco3':444.00
+    },
+    {
+        'item':'INSTALAÇÃO E MONTAGEM QDC (12 CIRCUITOS + DR + DPS)',
+        'preco':570.00,
+        'preco1':855.00,
+        'preco2':456.00,
+        'preco3':684.00
+    },
+    {
+        'item':'INSTALAÇÃO E MONTAGEM QDC (18 CIRCUITOS + DR + DPS)',
+        'preco':710.00,
+        'preco1':1065.00,
+        'preco2':568.00,
+        'preco3':852.00
+    },
+    {
+        'item':'INSTALAÇÃO E MONTAGEM QDC (24 CIRCUITOS + DR + DPS)',
+        'preco':950.00,
+        'preco1':1425.00,
+        'preco2':760.00,
+        'preco3':1140.00
+    }
+]
+passagemcabo20m = [
+    {
+        'item':'ENTRADA MONOFÁSICA ( QM PARA QDC)',
+        'preco1':150.00,
+        'preco3':120.00,
+        'preco2':0,
+        'preco':0
+    },
+    {
+        'item':'ENTRADA BIFÁSICA OU TRIFÁSICA ( QM PARA QDC)',
+        'preco1':190.00,
+        'preco3':152.00,
+        'preco2':0,
+        'preco':0
+    },
+    {
+        'item':'ALIMENTAÇÃO PARA AR CONDICIONADO',
+        'preco1':100.00,
+        'preco3':80.00,
+        'preco2':0,
+        'preco':0
+    },
+    {
+        'item':'ALIMENTAÇÃO PARA MOTORES',
+        'preco1':110.00,
+        'preco3':88.00,
+        'preco2':0,
+        'preco':0
+    },
+    {
+        'item':'CIRCUITO DE ATERRAMENTO C/ ATÉ 3 HASTES',
+        'preco1':110.00,
+        'preco3':88.00,
+        'preco2':0,
+        'preco':0
+    },
+    {
+        'item':'CIRCUITO INTERNO ATÉ 6 Metros',
+        'preco1':50.00,
+        'preco3':40.00,
+        'preco2':0,
+        'preco':0
+    },
+]
+soluceletrica = [
+    {
+        'item':'DE CURTO CIRCUITO MONOFÁSICO',
+        'preco':90.00,
+        'preco1':135.00,
+        'preco2':72.00,
+        'preco3':108.00
+    },
+    {
+        'item':'DE CURTO CIRCUITO BIFÁSICO',
+        'preco':110.00,
+        'preco1':165.00,
+        'preco2':88.00,
+        'preco3':132.00
+    },
+    {
+        'item':'CURTO CIRCUITO TRIFÁSICO',
+        'preco':130.00,
+        'preco1':195.00,
+        'preco2':104.00,
+        'preco3':156.00
+    },
+]
+servicos = [
+    'Encerrar orçamento e sair do programa',
+    'Iluminação',
+    'Pontos de ultilização (tomadas)',
+    'QDC (Quadros de distribuição) ',
+    'Passagem de cabos (*circuitos até 20 metros)',
+    'Soluções de problemas',
+    'Selecionar tabela de serviços para mostrar preços',
+    ]
 cond = 99
 cont = 1
 dados = list()
 orc = list()
 total = 0
 
+def menuCima():
+    print('-='*30)
+    print('')
+
+def menuBaixo():
+    print('')
+    print('-='*30)
+
+def separaCima():
+    print('-'*60)
+    print('')
+
+def separaBaixo():
+    print('')
+    print('-'*60)
+
+def detalhaPreco(lista):
+    menuCima()
+    print('COD    Tabela serviços')
+    menuBaixo()
+    for c in range(1,len(lista)-1):
+        print(f'{c}-{lista[c]}')
+    separaBaixo()
+    s = int(input('Digite o código do serviço para detalhar preços'))
+    return s
+    
+def mostrarLista(lista,msg):
+    menuCima()
+    print(f'          {msg}')
+    menuBaixo()
+    for v in range (0,len(lista)):
+        menuCima()
+        print(f'{lista[v]["item"]}')
+        menuBaixo()
+        print(f'sem passagem de cabos')
+        separaCima()
+        print(f'1 - UNIDADE R${lista[v]["preco"]:.2f}')
+        print(f'3 - ou + UNIDADES R${lista[v]["preco2"]:.2f}')
+        separaBaixo()
+        separaCima()
+        print('Com passagem de cabos')
+        print(f'1 - UNIDADE R${lista[v]["preco1"]:.2f}')
+        print(f'3 - ou + UNIDADES R${lista[v]["preco3"]:.2f}')
+        separaBaixo()
+    print('* Os preços acima são apenas uma base para orientação de valor por serviço dos profissionais da área elétrica.')
+    print('* Os valores podem variar de acordo com a região de cada profissional, é importante fazer uma pesquisa de mercado na sua região!')
+    print('* Os preços acima são o resultado de uma pesquisa com 2 mil votos de vários profissionais da área em diferentes regiões do país.')
+    print('* Os preços acima são referentes apenas ao valor da mão de obra, NÃO ESTÁ INCLUSO O MATERIAL necessário para execução.')
+    menuBaixo()
+
+def menu(lista,msg):
+    menuCima()
+    print(f'          {msg}')
+    menuBaixo()
+    for k, v in enumerate(lista):
+        print(f'{k}-{v["item"]}')
+    menuBaixo()
+
+
+
+
 while True:
     if cond == 0:
         break
-    print('Escolha uma opção abaixo para orçar ou digite 0 para encerrar o orçamento:\n')
-    print('1-Iluminação')
-    print('2-Pontos de ultilização (tomadas)')
-    print('3-QDC (Quadros de distribuição) ')
-    print('4-Passagem de cabos (*circuitos até 20 metros)')
-    print('5-Soluções de problemas')
+    menuCima()
+    print('              Menu principal')
+    menuBaixo()
+    print('Cod        Tabela Serviços')
+    separaCima()
+    for c in range(0,len(servicos)):
+        print(f'{c} - {servicos[c]}')
+    separaBaixo()
     cond = int(input('Digite o número correspondente\n'))
     if cond == 1:
-        while True:
-            print('-=*'*30)
-            print('cod            Serviço')
-            print('-'*60)
-            for k, v in enumerate(iluminacao):
-               print(f'{k} - {v["item"]}')
-            print('-=*'*30)
-            esc = int(input('Digite o codigo de um serviço para iniciar o orçamento: '))
-            print()
-            if esc > len(iluminacao):
-                print(f'Erro! Não existe serviço com o codigo {esc}')
-            else:
-                print('-='*30)
-                print(f'codigo do serviço: {esc} ')
-                print(f'Descrição do serviço:{iluminacao[esc]["item"]}')
-                print('-='*30)
-                print('')
-                print(f'Valor de 1 unidade sem passagem de cabos R${iluminacao[esc]["preco"]:.2f}')
-                print(f'Valor de 3 unidades sem passagem de cabos R${iluminacao[esc]["preco2"]:.2f}')
-                print(f'Valor de 1 unidade com passagem de cabos R${iluminacao[esc]["preco1"]:.2f}')
-                print(f'Valor de 3 unidades com passagem de cabosR${iluminacao[esc]["preco3"]:.2f}')
-                print('')
-                print('-='*30)
-                print('')
-                while True:
-                    print('')
-                    op = int(input(f'Digite a quantidade de {iluminacao[esc]["item"]} a serem intalados '))
-                    while True:
-                        print('')
-                        op2 = str(input(f'Total de {op} {iluminacao[esc]["item"]} a serem instaladas a quantidade está correta[S/N]? ')).upper()[0]
-                        if op2 in "SN":
-                            break
-                        else:
-                            print('Erro! Digite apenas sim ou não ')
-                    if op2 == "S":
-                        break
-            
-                while True:
-                    print('')
-                    op3 = str(input('Com passagem de cabos [S/N]')).upper()[0]
-                        
-                    if op3 in 'SN':
-                        break
-                    else:
-                        print('Erro! Digite apenas sim ou não.')
-                  
-                if op > 2 and op3 == 'N' :
-                    result = op * iluminacao[esc]["preco2"]
-                    print(iluminacao[esc]["preco2"])
-                elif op > 2 and op3 == "S":
-                    result = op * iluminacao[esc]["preco1"]
-                elif op3 == 'N':
-                    result = iluminacao[esc]["preco"]
-                else:
-                    result = iluminacao[esc]["preco3"]
-                dados.append(iluminacao[esc]["item"])
-                dados.append(op)
-                dados.append(op3)
-                dados.append(result)
-                orc.append(dados[:])
-                dados.clear()
-
-
-            while True:
-                print('')
-                resp = str(input('Deseja continuar em iluminação[S/N]?')).upper()[0]
-                if resp in "SN":
-                    break
-            if resp == 'N':
-                break 
+        menu(iluminacao,'iluminação')
+      
     elif cond == 2:
-        print('cod            Serviço')
-        print('-=*'*30)
-        for k, v in enumerate(pontoUtilizacao):
-            print(f'{k} - {v["item"]}')
-        print('')
-        print('-=*'*30)
-        print('')
-        # while True:
-        #     op = int(input('Digite um codigo para o serviço'))
-        #     if op > len(pontoUtilizacao):
-        #         print('Erro! não existe serviço com esse código')
-        #     else:
-        #         break
-        # while True:
-        #     op2 = int(input(f'Digite a quantidade de {pontoUtilizacao[op]["item"]}'))
-        #     op3 = str(input(f'Total de {op} - {pontoUtilizacao[op]["item"]} esta correta[S/N]?')).upper()[0]
-        #     if op3 == "S":
-        #         break
+        menu(pontoUtilizacao,'Ponto de ultilização')
+    elif cond == 3:
+        menu(qdc,'QDC')
+    elif cond == 4:
+        menu(passagemcabo20m, 'Passagem de cabos até 20m')
+    elif cond == 5:
+        menu(soluceletrica,'solução de problemas elétricos')
+    elif cond == 6:
+        c = detalhaPreco(servicos)
+        msg = servicos[c]
+        if c == 1:
+            mostrarLista(iluminacao,msg)
+        elif c ==2:
+                mostrarLista(pontoUtilizacao,msg)
+        elif c == 3:
+            mostrarLista(qdc,msg)
+        elif c == 4:
+            mostrarLista(passagemcabo20m,msg)
+        elif c == 5:
+            mostrarLista(soluceletrica,msg)
+
+    # if cond == 1:
+#         while True:
+#             print('-=*'*30)
+#             print('cod            Serviço')
+#             print('-'*60)
+#             for k, v in enumerate(iluminacao):
+#                print(f'{k} - {v["item"]}')
+#             print('-=*'*30)
+#             esc = int(input('Digite o codigo de um serviço para iniciar o orçamento: '))
+#             print()
+#             if esc > len(iluminacao):
+#                 print(f'Erro! Não existe serviço com o codigo {esc}')
+#             else:
+#                 print('-='*30)
+#                 print(f'codigo do serviço: {esc} ')
+#                 print(f'Descrição do serviço:{iluminacao[esc]["item"]}')
+#                 print('-='*30)
+#                 print('')
+#                 print(f'Valor de 1 unidade sem passagem de cabos R${iluminacao[esc]["preco"]:.2f}')
+#                 print(f'Valor de 3 unidades sem passagem de cabos R${iluminacao[esc]["preco2"]:.2f}')
+#                 print(f'Valor de 1 unidade com passagem de cabos R${iluminacao[esc]["preco1"]:.2f}')
+#                 print(f'Valor de 3 unidades com passagem de cabosR${iluminacao[esc]["preco3"]:.2f}')
+#                 print('')
+#                 print('-='*30)
+#                 print('')
+#                 while True:
+#                     print('')
+#                     op = int(input(f'Digite a quantidade de {iluminacao[esc]["item"]} a serem intalados '))
+#                     while True:
+#                         print('')
+#                         op2 = str(input(f'Total de {op} {iluminacao[esc]["item"]} a serem instaladas a quantidade está correta[S/N]? ')).upper()[0]
+#                         if op2 in "SN":
+#                             break
+#                         else:
+#                             print('Erro! Digite apenas sim ou não ')
+#                     if op2 == "S":
+#                         break
+            
+#                 while True:
+#                     print('')
+#                     op3 = str(input('Com passagem de cabos [S/N]')).upper()[0]
+                        
+#                     if op3 in 'SN':
+#                         break
+#                     else:
+#                         print('Erro! Digite apenas sim ou não.')
+                  
+#                 if op > 2 and op3 == 'N' :
+#                     result = op * iluminacao[esc]["preco2"]
+#                     print(iluminacao[esc]["preco2"])
+#                 elif op > 2 and op3 == "S":
+#                     result = op * iluminacao[esc]["preco1"]
+#                 elif op3 == 'N':
+#                     result = iluminacao[esc]["preco"]
+#                 else:
+#                     result = iluminacao[esc]["preco3"]
+#                 dados.append(iluminacao[esc]["item"])
+#                 dados.append(op)
+#                 dados.append(op3)
+#                 dados.append(result)
+#                 orc.append(dados[:])
+#                 dados.clear()
+
+
+#             while True:
+#                 print('')
+#                 resp = str(input('Deseja continuar em iluminação[S/N]?')).upper()[0]
+#                 if resp in "SN":
+#                     break
+#             if resp == 'N':
+#                 break 
+#     elif cond == 2:
+#         print('cod            Serviço')
+#         print('-=*'*30)
+#         for k, v in enumerate(pontoUtilizacao):
+#             print(f'{k} - {v["item"]}')
+#         print('')
+#         print('-=*'*30)
+#         print('')
+#         # while True:
+#         #     op = int(input('Digite um codigo para o serviço'))
+#         #     if op > len(pontoUtilizacao):
+#         #         print('Erro! não existe serviço com esse código')
+#         #     else:
+#         #         break
+#         # while True:
+#         #     op2 = int(input(f'Digite a quantidade de {pontoUtilizacao[op]["item"]}'))
+#         #     op3 = str(input(f'Total de {op} - {pontoUtilizacao[op]["item"]} esta correta[S/N]?')).upper()[0]
+#         #     if op3 == "S":
+#         #         break
        
 
-print('-='*30)
-print('')           
-print('Itens com passagem de cabos')
-print('-'*60)
-print('')                   
-for i in orc:
-    if i[2] == 'S':
-        print(f'{i[0]} x {i[1]} = {i[3]}')
-        total += i[3]
-print('-'*60)
-print('')
-print('Itens sem passagem de cabos')
-print('-'*60)
-print('')
-for i in orc:
-    if i[2] == 'N':
-        print(f'{i[0]} x {i[1]} = {i[3]}')
-        total += i[3]
-print('-'*60)
-print('')
-print('-='*30)
-print('')
-print(f'Valor total da mão de obra R${total:.2f}')
-print('')
-print('-='*30)
+# print('-='*30)
+# print('')           
+# print('Itens com passagem de cabos')
+# print('-'*60)
+# print('')                   
+# for i in orc:
+#     if i[2] == 'S':
+#         print(f'{i[0]} x {i[1]} = {i[3]}')
+#         total += i[3]
+# print('-'*60)
+# print('')
+# print('Itens sem passagem de cabos')
+# print('-'*60)
+# print('')
+# for i in orc:
+#     if i[2] == 'N':
+#         print(f'{i[0]} x {i[1]} = {i[3]}')
+#         total += i[3]
+# print('-'*60)
+# print('')
+# print('-='*30)
+# print('')
+# print(f'Valor total da mão de obra R${total:.2f}')
+# print('')
+# print('-='*30)
 
 
                      
