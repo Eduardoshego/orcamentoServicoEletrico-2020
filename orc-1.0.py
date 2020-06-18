0e45b2e321020ce2617bc2f757ceb4d6095eb8b1
Eduardo <eduardoshego1@gmail.com>
2020-06-18 11:22:08 -0300

    Finalizando funçao de realizar orçamento

--- orc-1.0.py
+++ orc-1.0.py
@@ -476,10 +476,10 @@ servicos = [
     'Selecionar tabela de serviços para mostrar preços',
     ]
 cond = 99
-cont = 1
-dados = list()
 orc = list()
-total = 0
+
+def erro():
+    print('Erro! Digite apenas sim ou não')
 
 def menuCima():
     print('-='*30)
@@ -536,12 +536,83 @@ def menu(lista,msg):
     print(f'          {msg}')
     menuBaixo()
     for k, v in enumerate(lista):
-        print(f'{k}-{v["item"]}')
+        print(f'{k} - {v["item"]}')
     menuBaixo()
+    s = int(input('Digite o código do serviço para orçar: '))
+    return s
 
 
+def calcula(lista,s,msg):
+    dados = list()
+    dados.append(lista[s]["item"])
+    while True:
+        while True:
+            quant = int(input(f'Digite a quantidade de {lista[s]["item"]}: '))
+            resp = str(input(f'São no total {quant} {lista[s]["item"]}. Está correta?[S/N]? ')).upper()[0]
+            if resp in "SN":
+                break
+            else:
+                erro()
 
+        if resp == 'S':
+            break
+    while True:
+        resp = str(input('Com passagem de cabos[S/N]')).upper()[0]
+        if resp in 'SN':
+            break
+        else:
+            erro()
+    dados.append(resp)
+    if quant > 2 and resp == 'S':
+        dados.append(quant)
+        dados.append(lista[s]["preco3"])
+        dados.append(lista[s]["preco3"]*quant)
+    elif quant > 2 and resp == 'N':
+        dados.append(quant)
+        dados.append(lista[s]["preco1"])
+        dados.append(lista[s]["preco1"]*quant)
+    elif quant <= 2 and resp == 'N':
+        dados.append(quant)
+        dados.append(lista[s]["preco"])
+        dados.append(lista[s]["preco"]*quant)
+    else:
+        dados.append(quant)
+        dados.append(lista[s]["preco2"])
+        dados.append(lista[s]["preco2"]*quant)
+    orc.append(dados[:])
+    dados.clear()
+    while True:
+        resp = str(input(f'Para continuar em {msg} Digite sim, para retornar ao meu principal digite não: ')).upper()[0]
+        if resp in 'SN':
+            break
+        else:
+            erro()
+    return resp
 
+def mostrarOrc():
+    menuCima()
+
+    print('Itens com passagem de cabos')
+    menuBaixo()
+    separaCima()
+    for c in range(0,len(orc)):
+        if orc[c][1] == 'S':
+            print(f'Item:{orc[c][0]} quantidade = {orc[c][2]} valor unitário = {orc[c][3]} valor total = {orc[c][4]}')
+    separaBaixo()
+    menuCima()
+    print('Itens sem passagem de cabos')
+    menuBaixo()
+    for c in range(0,len(orc)):
+        if orc[c][1] == 'N':
+            print(f'Item:{orc[c][0]} quantidade = {orc[c][2]} valor unitário = {orc[c][3]} valor total = {orc[c][4]}')
+    separaBaixo()
+    menuBaixo()
+    total = 0
+    for c in range(0,len(orc)):
+        total += orc[c][4]
+    print(f'Valor total = {total}')
+    
+  
 while True:
     if cond == 0:
         break
@@ -555,16 +626,36 @@ while True:
     separaBaixo()
     cond = int(input('Digite o número correspondente\n'))
     if cond == 1:
-        menu(iluminacao,'iluminação')
-      
+        while True:
+            s = menu(iluminacao,'iluminação')
+            resp = calcula(iluminacao,s,servicos[1])
+            if resp == 'N':
+                break
     elif cond == 2:
-        menu(pontoUtilizacao,'Ponto de ultilização')
+        while True:
+            s = menu(pontoUtilizacao,'Ponto de ultilização')
+            resp = calcula(pontoUtilizacao,s,servicos[2])
+            if resp == 'N':
+                break
+
     elif cond == 3:
-        menu(qdc,'QDC')
+        while True:
+            s = menu(qdc,'QDC')
+            resp = calcula(qdc,s,servicos[3])
+            if resp == 'N':
+                break
     elif cond == 4:
-        menu(passagemcabo20m, 'Passagem de cabos até 20m')
+        while True:
+            s = menu(passagemcabo20m, 'Passagem de cabos até 20m')
+            resp = calcula(passagemcabo20m,s,servicos[4])
+            if resp == 'N':
+                break
     elif cond == 5:
-        menu(soluceletrica,'solução de problemas elétricos')
+        while True:
+            s = menu(soluceletrica,'solução de problemas elétricos')
+            resp = calcula(soluceletrica,s,servicos[5])
+            if resp == 'N':
+                break
     elif cond == 6:
         c = detalhaPreco(servicos)
         msg = servicos[c]
@@ -578,125 +669,7 @@ while True:
             mostrarLista(passagemcabo20m,msg)
         elif c == 5:
             mostrarLista(soluceletrica,msg)
-
-    # if cond == 1:
-#         while True:
-#             print('-=*'*30)
-#             print('cod            Serviço')
-#             print('-'*60)
-#             for k, v in enumerate(iluminacao):
-#                print(f'{k} - {v["item"]}')
-#             print('-=*'*30)
-#             esc = int(input('Digite o codigo de um serviço para iniciar o orçamento: '))
-#             print()
-#             if esc > len(iluminacao):
-#                 print(f'Erro! Não existe serviço com o codigo {esc}')
-#             else:
-#                 print('-='*30)
-#                 print(f'codigo do serviço: {esc} ')
-#                 print(f'Descrição do serviço:{iluminacao[esc]["item"]}')
-#                 print('-='*30)
-#                 print('')
-#                 print(f'Valor de 1 unidade sem passagem de cabos R${iluminacao[esc]["preco"]:.2f}')
-#                 print(f'Valor de 3 unidades sem passagem de cabos R${iluminacao[esc]["preco2"]:.2f}')
-#                 print(f'Valor de 1 unidade com passagem de cabos R${iluminacao[esc]["preco1"]:.2f}')
-#                 print(f'Valor de 3 unidades com passagem de cabosR${iluminacao[esc]["preco3"]:.2f}')
-#                 print('')
-#                 print('-='*30)
-#                 print('')
-#                 while True:
-#                     print('')
-#                     op = int(input(f'Digite a quantidade de {iluminacao[esc]["item"]} a serem intalados '))
-#                     while True:
-#                         print('')
-#                         op2 = str(input(f'Total de {op} {iluminacao[esc]["item"]} a serem instaladas a quantidade está correta[S/N]? ')).upper()[0]
-#                         if op2 in "SN":
-#                             break
-#                         else:
-#                             print('Erro! Digite apenas sim ou não ')
-#                     if op2 == "S":
-#                         break
-            
-#                 while True:
-#                     print('')
-#                     op3 = str(input('Com passagem de cabos [S/N]')).upper()[0]
-                        
-#                     if op3 in 'SN':
-#                         break
-#                     else:
-#                         print('Erro! Digite apenas sim ou não.')
-                  
-#                 if op > 2 and op3 == 'N' :
-#                     result = op * iluminacao[esc]["preco2"]
-#                     print(iluminacao[esc]["preco2"])
-#                 elif op > 2 and op3 == "S":
-#                     result = op * iluminacao[esc]["preco1"]
-#                 elif op3 == 'N':
-#                     result = iluminacao[esc]["preco"]
-#                 else:
-#                     result = iluminacao[esc]["preco3"]
-#                 dados.append(iluminacao[esc]["item"])
-#                 dados.append(op)
-#                 dados.append(op3)
-#                 dados.append(result)
-#                 orc.append(dados[:])
-#                 dados.clear()
+mostrarOrc()
 
 
-#             while True:
-#                 print('')
-#                 resp = str(input('Deseja continuar em iluminação[S/N]?')).upper()[0]
-#                 if resp in "SN":
-#                     break
-#             if resp == 'N':
-#                 break 
-#     elif cond == 2:
-#         print('cod            Serviço')
-#         print('-=*'*30)
-#         for k, v in enumerate(pontoUtilizacao):
-#             print(f'{k} - {v["item"]}')
-#         print('')
-#         print('-=*'*30)
-#         print('')
-#         # while True:
-#         #     op = int(input('Digite um codigo para o serviço'))
-#         #     if op > len(pontoUtilizacao):
-#         #         print('Erro! não existe serviço com esse código')
-#         #     else:
-#         #         break
-#         # while True:
-#         #     op2 = int(input(f'Digite a quantidade de {pontoUtilizacao[op]["item"]}'))
-#         #     op3 = str(input(f'Total de {op} - {pontoUtilizacao[op]["item"]} esta correta[S/N]?')).upper()[0]
-#         #     if op3 == "S":
-#         #         break
-       
-
-# print('-='*30)
-# print('')           
-# print('Itens com passagem de cabos')
-# print('-'*60)
-# print('')                   
-# for i in orc:
-#     if i[2] == 'S':
-#         print(f'{i[0]} x {i[1]} = {i[3]}')
-#         total += i[3]
-# print('-'*60)
-# print('')
-# print('Itens sem passagem de cabos')
-# print('-'*60)
-# print('')
-# for i in orc:
-#     if i[2] == 'N':
-#         print(f'{i[0]} x {i[1]} = {i[3]}')
-#         total += i[3]
-# print('-'*60)
-# print('')
-# print('-='*30)
-# print('')
-# print(f'Valor total da mão de obra R${total:.2f}')
-# print('')
-# print('-='*30)
-
-
-                     
     
